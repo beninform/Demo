e4b09542e0e282a7827c173cd3a06ea02bb440d7
beninform <benwilson5443@yahoo.co.uk>
2026-07-20 23:41:19 +0100

    Update skip-button.js
    finalizeSession function moved to separate data-save js file.

--- web/skip-button.js
+++ web/skip-button.js
@@ -352,56 +352,9 @@ function handleTrialFinish(data) {
     }
 }
 
-function finalizeSession(status = 'NORMAL') {
-    let prefix = uid + '_' + tid + '_' + pid;
-    let dataPipeExperimentId = 'FP1EmrkIikGE';
-    let forceOSFSave = false;
 
-    // Filter and retrieve results as CSV data
-    let dataObj = jsPsych.data
-        .get()
-        .filter({ collect: true })
-        .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect', 'time_elapsed']);
+// NB - finalizeSession() function moved to data-save.js file
 
-    let results = dataObj.csv();
-
-    // Generate a current timestamp for the filename
-    let timestamp = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
-
-    dataObj.localSave('csv', prefix + '_' + status + '-' + timestamp + '.csv');
-
-    // Dynamically determine if the experiment is currently running locally or on production
-    let isLocalHost = window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1');
-
-    let destination = '/save';
-    if (!isLocalHost || forceOSFSave) {
-        destination = 'https://pipe.jspsych.org/api/data/';
-    };
-
-    // Send the results to our saving end point
-    fetch(destination, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: '*/*',
-        },
-        body: JSON.stringify({
-            experimentID: dataPipeExperimentId,
-            filename: prefix + '_' + status + '-' + timestamp + '.csv',
-            data: results,
-        }),
-    }).then(data => {
-        if (status === 'NORMAL') {
-            jsPsych.finishTrial();
-        }
-    })
-
-    if (status === 'ABORT_TIMEOUT') {
-        jsPsych.abortExperiment(trialText.abortTimeoutText);
-    } else if (status === 'ABORT_SKIP') {
-        jsPsych.abortExperiment(trialText.abortSkipText);
-    }
-}
 
 // Function to show the skip button after 1 minute (60,000 ms)
 function showSkipButton() {
